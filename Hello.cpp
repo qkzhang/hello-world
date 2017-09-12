ea701d31be308730b064bdf475cbb8ba548474e6
qkzhang <qkzhang@quant360.com>
2017-09-12 16:22:15 +0800

    test: rebase test

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+#include	<iostream>
+#include	<string.h>
+
+int main () {
+
+	printf("rebase test");	
+	printf("another rebase test!");
+	printf("main Function!\r\n");
+	return 1;
+}
+
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include	<stdio.h>
+#include	<string.h>
+
+void testFunc() {
+
+	printf("we are in the test function!");
+	printf("we are ready!");
+
+}
